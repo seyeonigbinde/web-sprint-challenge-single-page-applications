c8a9a23d62eebb3e3130f919d14c1d2493ed4902
Your Name <you@example.com>
2021-04-23 20:24:17 +0100

    Test to submit the form done on Cypress

--- cypress/integration/examples/form_tests.js
+++ cypress/integration/examples/form_tests.js
@@ -7,12 +7,11 @@ describe("Quotes app", () => {
 const nameInput = () => cy.get('input[name="name"]');
 const sizeInput = () => cy.get('#size-dropdown');
 const choiceInput = () => cy.get('input[name="choice"]');
-
 const checkboxInput = () => cy.get('input[type="checkbox"]');
 const specialInput = () => cy.get('#special-text');
-// const submitBtn = () => cy.get('button[id="order-button"]');
+const submitBtn = () => cy.get('button[id="order-button"]');
 
-it("Can type in the inputs", () => {
+it("test that you can add text to the box", () => {
      nameInput()
         .should("have.value", "")
         .type("Seye Onigbinde")
@@ -25,17 +24,48 @@ it("Can type in the inputs", () => {
         choiceInput()
         .check("garlic_ranch");
 
+        specialInput()
+        .should("have.value", "")
+        .type("Please deliver on time")
+        .should("have.value", "Please deliver on time");
+
+    });
+
+    it("test that you can select multiple toppings", () => {
+
         checkboxInput()
         .should("not.be.checked")
         .check(['topping1', 'topping3'])
         .should("be.checked");
-   
+
+    });
+
+    it("test that you can submit the form", () => {
+
+        nameInput()
+        .should("have.value", "")
+        .type("Seye Onigbinde")
+        .should("have.value", "Seye Onigbinde");
+
+        sizeInput()
+        .select("small")
+        .should("have.value", "small");
+
+        choiceInput()
+        .check("garlic_ranch");
 
         specialInput()
         .should("have.value", "")
         .type("Please deliver on time")
         .should("have.value", "Please deliver on time");
 
+
+        checkboxInput()
+        .should("not.be.checked")
+        .check(['topping1', 'topping3'])
+        .should("be.checked");
+
+        submitBtn().click();
 
     });
 
